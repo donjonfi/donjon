ed27d99548817da98bbd2f1584c0d2645dfcd240
Jonathan Claes <stuut@xax.be>
2020-06-01 15:58:23 +0200

    dev: classe et états;

---
+++ webapp/donjon-ide/src/app/models/commun/classe.ts
@@ -0,0 +1,8 @@
+import { Genre } from './genre.enum';
+
+export class Classe {
+
+  parent: Classe;
+  intitule: string;
+  etats: Etat[];
+}
---
+++ webapp/donjon-ide/src/app/models/commun/etat.ts
@@ -0,0 +1,11 @@
+
+export class Etat {
+
+  intitule: string;
+  intituleMS: string;
+  intituleMP: string;
+  intituleFS: string;
+  intituleFP: string;
+  inverse: string;
+  valeur: boolean;
+}
