9e15282ba1566d98a7b18444e53db9cb1c9afd6c
Jonathan Claes <stuut@xax.be>
2020-10-11 09:46:22 +0200

    add: assets: commandes de base; upd: coloration syntaxique (Geshi);

--- ressources/wiki/Geshi/donjon.php
+++ ressources/wiki/Geshi/donjon.php
@@ -44,7 +44,7 @@ $language_data = array (
             'action', 'objet', 'lieu', 'support', 'contenant', 'porte', 'vivant', 'élément', 'décor', 'animal', 'personne'
             ),
         3 => array(
-            'si', 'remplacer', 'exécuter', 'refuser', 'terminer', 'peut', 'est', 'sont', 'pas', 'plus', 'possède'
+            'si', 'sinon', 'remplacer', 'exécuter', 'refuser', 'terminer', 'peut', 'est', 'sont', 'pas', 'plus', 'possède', 'contient'
             ),
         4 => array(
             'intitulé','accord','joueur', 'ceci', 'cela', 'ici', 'description', 'nom', 'titre'
