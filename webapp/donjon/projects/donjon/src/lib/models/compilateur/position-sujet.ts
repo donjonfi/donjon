4af12b4669e98d3e5c058d698ce25d310abb65cf
Jonathan Claes <stuut@xax.be>
2020-12-12 14:35:47 +0100

    rem: classe position sujet inutilisée;

--- webapp/donjon/projects/donjon/src/lib/models/compilateur/position-sujet.ts
+++ webapp/donjon/projects/donjon/src/lib/models/compilateur/position-sujet.ts
@@ -1,29 +1,5 @@
 import { ElementDonjon } from './element-donjon';
 
-export enum PositionRelative {
-  /** nord */
-  nord = 's',
-  /** est */
-  est = 'e',
-  /** sud */
-  sud = 's',
-  /** ouest */
-  ouest = 'o',
-  /** intérieur */
-  interieur = 'i',
-  /** extérieur */
-  exterieur = 'x',
-}
-
-export class PositionSujet {
-  constructor(
-    public sujet: ElementDonjon,
-    public complement: ElementDonjon,
-    /** Position du sujet relative au complément */
-    public position: PositionRelative,
-  ) { }
-}
-
 export class PositionSujetString {
   constructor(
     public sujet: string,
