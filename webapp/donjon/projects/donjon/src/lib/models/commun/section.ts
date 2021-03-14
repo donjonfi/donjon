3294a9f5007f1bedc8351f2bb86214602bc58c1b
Jonathan Claes <stuut@xax.be>
2021-03-14 19:49:12 +0100

    add: type section;

---
+++ webapp/donjon/projects/donjon/src/lib/models/commun/section.ts
@@ -0,0 +1,12 @@
+
+export class Section {
+
+  /** Ligne de la section dans le scénario. */
+  ligne: number;
+
+  /** Intitulé de la section dans le scénario. */
+  intitule: string;
+
+  /** Section parent */
+  parent: Section;
+}
