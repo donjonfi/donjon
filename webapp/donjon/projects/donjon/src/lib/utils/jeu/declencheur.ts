5c1a6a3a48d2e8e0094787495aec32636128296b
zigotteau <stuut@xax.be>
2021-04-14 10:03:08 +0200

    déclancheurs: retrouverInstruction: new: ceci et cela supportés (déclancheurs génériques); upd: calcul du score de priorité;

--- webapp/donjon/projects/donjon/src/lib/utils/jeu/declencheur.ts
+++ webapp/donjon/projects/donjon/src/lib/utils/jeu/declencheur.ts
@@ -40,9 +40,12 @@ export class Declencheur {
 
     let scoreAuditeursDeclanches: [Auditeur, number][] = [];
 
-    const scoreCorrespondanceExact: number = 1000;
-    const scoreCorrespondanceSemiExact: number = 500;
-    const scoreCorrespondanceClasse: number = 1;
+    const scoreCorrespondanceExact: number = 4000;
+    const scoreCorrespondanceSemiExact: number = 2000;
+    const scoreCorrespondanceClasse: number = 1000;
+    const scoreCorrespondanceSemiClasse: number = 500;
+    const scoreCorrespondanceCeciCela: number = 250;
+    const scoreCorrespondanceSemiCeciCela: number = 125;
 
     let meilleurScore = 0;
 
@@ -52,64 +55,93 @@ export class Declencheur {
       aud.evenements.forEach(curAudEvenement => {
         // vérifier infinitif
         if (curAudEvenement.infinitif === evenement.infinitif) {
-          // A) aucun argument
+          // A) AUCUN ARGUMENT
           if (!evenement.isCeci && !evenement.isCela) {
             // même type d’évènement ?
             if (!curAudEvenement.isCeci && !curAudEvenement.isCela) {
               meilleurScorePourCetAuditeur = scoreCorrespondanceExact;
             }
-            // B) ceci uniquement
+            // B) CECI UNIQUEMENT
           } else if (evenement.isCeci && !evenement.isCela) {
             // même type d’évènement ?
             if (curAudEvenement.isCeci && !curAudEvenement.isCela) {
-              // classe => vérifier si la classe hérite
+              // ––––––––––––––––––––––––––––––
+              // a. ceci: classe
+              // ––––––––––––––––––––––––––––––
               if (curAudEvenement.classeCeci !== null) {
                 if (ClasseUtils.heriteDe(evenement.classeCeci, curAudEvenement.classeCeci.nom)) {
                   let curScore = scoreCorrespondanceClasse + curAudEvenement.classeCeci.niveau;
                   meilleurScorePourCetAuditeur = Math.max(curScore, meilleurScorePourCetAuditeur);
                 }
-                // élément => vérifier si l’élément exact correspond
+                // ––––––––––––––––––––––––––––––
+                // b. ceci: 'ceci' générique (on accepte aussi 'cela')
+                // ––––––––––––––––––––––––––––––
+              } else if (curAudEvenement.ceci === 'ceci' || curAudEvenement.ceci === 'cela') {
+                let curScore = scoreCorrespondanceCeciCela;
+                meilleurScorePourCetAuditeur = Math.max(curScore, meilleurScorePourCetAuditeur);
+                // ––––––––––––––––––––––––––––––
+                // c. ceci: élément
+                // ––––––––––––––––––––––––––––––
               } else if (curAudEvenement.ceci === evenement.ceci) {
                 meilleurScorePourCetAuditeur = scoreCorrespondanceExact;
               }
             }
-            // C) cela uniquement
+            // C) CELA UNIQUEMENT => NE DEVRAIT JAMAIS SE PRODUIRE
           } else if (!evenement.isCeci && evenement.isCela) {
-            // même type d’évènement ?
-            if (!curAudEvenement.isCeci && curAudEvenement.isCela) {
-              // classe => vérifier si la classe hérite
-              if (curAudEvenement.classeCela !== null) {
-                if (ClasseUtils.heriteDe(evenement.classeCela, curAudEvenement.classeCela.nom)) {
-                  let curScore = scoreCorrespondanceClasse + curAudEvenement.classeCela.niveau;
-                  meilleurScorePourCetAuditeur = Math.max(curScore, meilleurScorePourCetAuditeur);
-                }
-                // élément => vérifier si l’élément exact correspond
-              } else if (curAudEvenement.cela === evenement.cela) {
-                meilleurScorePourCetAuditeur = scoreCorrespondanceExact;
-              }
-            }
-            // D) ceci et cela
+            console.error("Déclancheur ne peut pas se faire sur « cela » uniquement. Seuls « (rien) », « ceci » et « ceci et cela » sont autorisés.");
+            // D) CECI ET CELA
           } else {
             // même type d’évènement ?
             if (curAudEvenement.isCeci && curAudEvenement.isCela) {
+              // ––––––––––––––––––––––––––––––
               // a. ceci: élément, cela: élément
+              // ––––––––––––––––––––––––––––––
               if (!curAudEvenement.classeCeci && !curAudEvenement.classeCela) {
-                if (curAudEvenement.ceci === evenement.ceci && curAudEvenement.cela === curAudEvenement.cela) {
+                // a.1 ceci: 'ceci' générique, cela: 'cela' générique (on accepte aussi 'cela' et 'ceci')
+                if ((curAudEvenement.ceci === 'ceci' || curAudEvenement.ceci === 'cela') && (curAudEvenement.cela === 'cela' || curAudEvenement.cela === 'ceci')) {
+                  let curScore = scoreCorrespondanceCeciCela;
+                  meilleurScorePourCetAuditeur = Math.max(curScore, meilleurScorePourCetAuditeur);
+                  // a.2 ceci: 'ceci' générique, cela: élément
+                } else if ((curAudEvenement.ceci === 'ceci' || curAudEvenement.ceci === 'cela') && curAudEvenement.cela === evenement.cela) {
+                  let curScore = scoreCorrespondanceSemiCeciCela + scoreCorrespondanceSemiExact;
+                  meilleurScorePourCetAuditeur = Math.max(curScore, meilleurScorePourCetAuditeur);
+                  // a.3 ceci: élément, cela: 'cela' générique
+                } else if (curAudEvenement.ceci === evenement.ceci && (curAudEvenement.cela === 'cela' || curAudEvenement.cela === 'ceci')) {
+                  let curScore = scoreCorrespondanceSemiExact + scoreCorrespondanceSemiCeciCela;
+                  meilleurScorePourCetAuditeur = Math.max(curScore, meilleurScorePourCetAuditeur);
+                  // a.4 ceci: élément, cela: élément
+                } else if (curAudEvenement.ceci === evenement.ceci && curAudEvenement.cela === evenement.cela) {
                   meilleurScorePourCetAuditeur = scoreCorrespondanceExact;
                 }
+                // ––––––––––––––––––––––––––––––
                 // b. ceci: élément, cela: classe
+                // ––––––––––––––––––––––––––––––
               } else if (!curAudEvenement.classeCeci) {
-                if (curAudEvenement.ceci === evenement.ceci && ClasseUtils.heriteDe(evenement.classeCela, curAudEvenement.classeCela.nom)) {
-                  let curScore = scoreCorrespondanceSemiExact + curAudEvenement.classeCela.niveau;
+                // b.1 ceci: 'ceci' générique, cela: classe
+                if ((curAudEvenement.ceci === 'ceci' || curAudEvenement.ceci === 'cela') && ClasseUtils.heriteDe(evenement.classeCela, curAudEvenement.classeCela.nom)) {
+                  let curScore = scoreCorrespondanceSemiCeciCela + scoreCorrespondanceSemiClasse + curAudEvenement.classeCela.niveau;
+                  meilleurScorePourCetAuditeur = Math.max(curScore, meilleurScorePourCetAuditeur);
+                  // b.2 ceci: élément, cela: classe
+                } else if (curAudEvenement.ceci === evenement.ceci && ClasseUtils.heriteDe(evenement.classeCela, curAudEvenement.classeCela.nom)) {
+                  let curScore = scoreCorrespondanceSemiExact + scoreCorrespondanceSemiClasse + curAudEvenement.classeCela.niveau;
                   meilleurScorePourCetAuditeur = Math.max(curScore, meilleurScorePourCetAuditeur);
                 }
+                // ––––––––––––––––––––––––––––––
                 // c. ceci: classe, cela: élément
+                // ––––––––––––––––––––––––––––––
               } else if (!curAudEvenement.classeCela) {
-                if (ClasseUtils.heriteDe(evenement.classeCeci, curAudEvenement.classeCeci.nom) && curAudEvenement.cela === curAudEvenement.cela) {
-                  let curScore = scoreCorrespondanceSemiExact + curAudEvenement.classeCeci.niveau;
+                // c.1 ceci: classe, cela: 'cela' générique
+                if (ClasseUtils.heriteDe(evenement.classeCeci, curAudEvenement.classeCeci.nom) && (curAudEvenement.cela === 'cela' || curAudEvenement.cela === 'ceci')) {
+                  let curScore = scoreCorrespondanceSemiClasse + curAudEvenement.classeCeci.niveau + scoreCorrespondanceSemiCeciCela;
+                  meilleurScorePourCetAuditeur = Math.max(curScore, meilleurScorePourCetAuditeur);
+                  // c.2 ceci: classe, cela: élément
+                } else if (ClasseUtils.heriteDe(evenement.classeCeci, curAudEvenement.classeCeci.nom) && curAudEvenement.cela === evenement.cela) {
+                  let curScore = scoreCorrespondanceSemiExact + scoreCorrespondanceSemiClasse + curAudEvenement.classeCeci.niveau;
                   meilleurScorePourCetAuditeur = Math.max(curScore, meilleurScorePourCetAuditeur);
                 }
+                // ––––––––––––––––––––––––––––––
                 // d. ceci: classe, cela: classe
+                // ––––––––––––––––––––––––––––––
               } else {
                 if (ClasseUtils.heriteDe(evenement.classeCeci, curAudEvenement.classeCeci.nom) && ClasseUtils.heriteDe(evenement.classeCela, curAudEvenement.classeCela.nom)) {
                   let curScore = scoreCorrespondanceClasse + Math.max(curAudEvenement.classeCeci.niveau, curAudEvenement.classeCela.niveau);
@@ -118,7 +150,6 @@ export class Declencheur {
               }
             }
           }
-
         }
       });
 
