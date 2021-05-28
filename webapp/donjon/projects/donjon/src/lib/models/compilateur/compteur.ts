91904f31ead4f4772ef85be37c3b34f15df876be
zigotteau <stuut@xax.be>
2021-05-28 23:51:35 +0200

    upd: classe compteur;

--- webapp/donjon/projects/donjon/src/lib/models/compilateur/compteur.ts
+++ webapp/donjon/projects/donjon/src/lib/models/compilateur/compteur.ts
@@ -1,9 +1,18 @@
+import { Classe } from "../commun/classe";
+import { ClassesRacines } from "../commun/classes-racines";
+import { GroupeNominal } from "../commun/groupe-nominal";
 import { Intitule } from "../jeu/intitule";
 
 export class Compteur extends Intitule {
 
-    /** Valeur du compteur */
-    public valeur: number;
+    constructor(
+        nom: string,
+        /** Valeur du compteur */
+        public valeur: number = 0,
+        intitule: GroupeNominal = null,
+        classe: Classe = ClassesRacines.Compteur,
+    ) {
+        super(nom, (intitule ? intitule : (new GroupeNominal(null, nom, null))), classe);
+    }
 
-    
 }
