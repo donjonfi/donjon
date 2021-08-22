754b9a2302b0b90e2902edc88a6a38c072ebe35f
zigotteau <stuut@xax.be>
2021-08-22 18:50:36 +0200

    éditeur: upd: mise en surbrillance (auteurs, infinitif, préposition)

--- webapp/donjon/projects/donjon-creer/src/mode-donjon.js
+++ webapp/donjon/projects/donjon-creer/src/mode-donjon.js
@@ -8,7 +8,7 @@ ace.define("ace/mode/donjon_highlight_rules", ["require", "exports", "module", "
 
     var variableLanguage = (
       "joueur|inventaire|historique|"
-      + "intitulé|description|titre|auteur|aperçu|texte|lien|capacité|accord|réaction|version|licence|jeu|"
+      + "intitulé|description|infinitif|préposition|titre|auteur|auteurs|aperçu|texte|lien|capacité|accord|réaction|version|licence|site|web|jeu|"
       + "aide|commande|action|contenu"
     );
 
