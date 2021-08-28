ec69cbf1fee5d2328d4764a33036e5233286188e
zigotteau <stuut@xax.be>
2021-08-28 21:14:19 +0200

    mise en surbrillance : instruction « activer »;

--- webapp/donjon/projects/donjon-creer/src/mode-donjon.js
+++ webapp/donjon/projects/donjon-creer/src/mode-donjon.js
@@ -58,7 +58,7 @@ ace.define("ace/mode/donjon_highlight_rules", ["require", "exports", "module", "
         },
         {
           token: "keyword",
-          regex: "(\\b(désactiver|quand|avant(?!\\;|\\.)|après(?!\\;|\\.)|si|sinon|sinonsi|fin si|finsi|refuser|(exécuter|terminer)(?! (l’|l'|la |le )?(action|commande|jeu))|interpréter|comme)\\b)|" +
+          regex: "(\\b(désactiver|activer|quand|avant(?!\\;|\\.)|après(?!\\;|\\.)|si|sinon|sinonsi|fin si|finsi|refuser|(exécuter|terminer)(?! (l’|l'|la |le )?(action|commande|jeu))|interpréter|comme)\\b)|" +
             "(\\b(au hasard|en boucle|1ère fois|1ere fois|1re fois|[1-9][0-9]?e fois|[1-9][0-9]?ème fois|[1-9][0-9]?eme fois|initialement|prioritairement|puis|fin choix|finchoix)\\b)|" +
             "(\\b(mais pas|mais bien|mais ni|mais soit|mais plus|ainsi que|et|ou|ni|soit)\\b)|" +
             "(\\b(partie|chapitre|scène) )",
