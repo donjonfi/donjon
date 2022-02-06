0baaf61fc7572931b103761c6ed815b21ce2f4b1
zigotteau <stuut@xax.be>
2022-02-07 00:01:16 +0100

    upd version (302)

--- webapp/donjon/projects/donjon/src/lib/models/commun/constantes.ts
+++ webapp/donjon/projects/donjon/src/lib/models/commun/constantes.ts
@@ -1,6 +1,6 @@
 
-export const version = "0.301 beta"
-export const versionNum = 301;
+export const version = "0.302 beta"
+export const versionNum = 302;
 
 export enum EClasseRacine {
   intitule = 'intitule',
