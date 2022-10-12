78cb5ca74acd52725bbb3138d36c1feb940450d2
zigotteau <stuut@xax.be>
2022-10-12 23:50:10 +0200

    màj version (beta 5)

--- webapp/donjon/projects/donjon/src/lib/models/commun/constantes.ts
+++ webapp/donjon/projects/donjon/src/lib/models/commun/constantes.ts
@@ -1,6 +1,6 @@
 
-export const version = "2.0-beta.4"
-export const versionNum = 1004;
+export const version = "2.0-beta.5"
+export const versionNum = 1005;
 
 export enum EClasseRacine {
   intitule = 'intitule',
