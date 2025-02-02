0807cc087e70fa968072d5f5f84668e96fb1ee81
donjonfi <stuut@xax.be>
2025-02-02 12:04:56 +0100

    màj version (3.2.0)

--- webapp/donjon/projects/donjon/src/lib/models/commun/constantes.ts
+++ webapp/donjon/projects/donjon/src/lib/models/commun/constantes.ts
@@ -1,6 +1,6 @@
 
-export const version = "3.1.1"
-export const versionNum = 30101;
+export const version = "3.2.0"
+export const versionNum = 30200;
 
 export enum EClasseRacine {
   intitule = 'intitule',
