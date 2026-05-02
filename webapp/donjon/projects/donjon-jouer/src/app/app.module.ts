eeb2352de5760d12143c7a6152bb8745e4208a84
Jonathan Claes <stuut@xax.be>
2026-05-02 20:40:27 +0200

    cor package.json & modules

--- webapp/donjon/projects/donjon-jouer/src/app/app.module.ts
+++ webapp/donjon/projects/donjon-jouer/src/app/app.module.ts
@@ -24,7 +24,7 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
     BrowserAnimationsModule,
     FormsModule,
     AppRoutingModule,
-    TooltipModule.forRoot(),
+    TooltipModule,
     DonjonModule
   ],
   providers: [
