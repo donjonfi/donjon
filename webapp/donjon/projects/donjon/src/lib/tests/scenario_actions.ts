aaf755f518fec1554373022c27372121413e78fd
Jonathan Claes <stuut@xax.be>
2026-06-20 18:31:17 +0200

    màj version actions

--- webapp/donjon/projects/donjon/src/lib/tests/scenario_actions.ts
+++ webapp/donjon/projects/donjon/src/lib/tests/scenario_actions.ts
@@ -1,6 +1,6 @@
 ﻿
 export const actions = `-- Titre: "Actions de base pour Donjon FI".
--- Version: 2026-06-19-31100
+-- Version: 2026-06-20-31100
 -- Auteur: Jonathan Claes (https://donjon.fi)
 -- Licence: Ce fichier est offert sous licence
 --   "Creative Commons Attribution 4.0 International License".
